cf443848610c15cf9f2804795258d7dab40ea91c
Gilbert Marpuri <gilbertmarpuri@artistryware.com>
2018-08-14 21:20:37 +0800

    Removed unnecessary buttons Added flipping of all cards backwards Added comparing of two cards flipped Added new field for struct Card

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -15,31 +15,33 @@ class ViewController: UIViewController {
         var button: UIButton
         var buttonTitle: String
         var matched: Bool
+        var touched: Bool
     }
     
-    var firstPick: Card!
-    var secondPick: Card!
+    var numberOfTouchedCard = 0
+    var previousCardIndex: Int!
     
     var emojiStrings = ["🐮","🐶","🦊","🐸","🍔","🍟","🍕","🍗"]
     
     var cardsWithEmoji = [String]()
+    var isFlippingCurrently = false
 
     @IBOutlet weak var card1: UIButton!
     @IBOutlet weak var card2: UIButton!
     
     var cards = [Card]()
     
-    var numberOfCards = 2
-    let MIN_NUMBER_OF_CARDS = 2
+    var numberOfCards = 4
+    let MIN_NUMBER_OF_CARDS = 4
     let MAX_NUMBER_OF_CARDS = 10
     
     var xPosArrayForTwoRows = [90, 210]
     
-    func pickRandomElements(in arr: [String], by numberOfCards: Int) -> [String]
+    func pickRandomElements(in stringArray: [String], by numberOfCards: Int) -> [String]
     {
         var sequenceOfStrings = [String]()
         var listOfStringsToUse = [String]()
-        var originalListOfStrings = arr //temporarily holds the list
+        var originalListOfStrings = stringArray //temporarily holds the list
         
         let halfOfTheNumberOfCards = numberOfCards / 2
         
@@ -77,34 +79,12 @@ class ViewController: UIViewController {
         return sequencedString
     }
     
-    
-    
-    @IBAction func deleteButtons(_ sender: UIButton) {
-        for c in cards {
-            c.button.removeFromSuperview()
+    @IBAction func minusButtonAction(_ sender: UIButton) {
+        for card in cards {
+            card.button.removeFromSuperview()
         }
         cards.removeAll()
-        
-        numberOfCards += 2
-        if (numberOfCards > 10) {
-            numberOfCards = 10
-        }
-        
-        createCards(by: numberOfCards)
-    }
-    
-    @IBAction func createButtons(_ sender: UIButton){
-        for c in cards {
-            c.button.removeFromSuperview()
-        }
-        cards.removeAll()
-        
-        numberOfCards -= 2
-        if (numberOfCards < 2) {
-            numberOfCards = 2
-        }
-        
-        createCards(by: numberOfCards)
+        createCards(by: 4)
     }
     
     func createCards(by value: Int) {
@@ -112,7 +92,7 @@ class ViewController: UIViewController {
         let stringListForCards = pickRandomElements(in: emojiStrings, by: value)
         
         var xPos = 0
-        var yPos = 90
+        var yPos = 50
         var xIndexer = 0
         var cardIndex = 1
         
@@ -128,72 +108,100 @@ class ViewController: UIViewController {
             
             let myButton = UIButton(frame: CGRect(x: xPos, y: yPos, width: 80, height: 80))
             
-            myButton.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
-            myButton.setTitle(titleOfCard, for: UIControlState.normal)
+            myButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            myButton.setTitle("", for: UIControlState.normal)
             myButton.titleLabel?.font = UIFont.systemFont(ofSize: 50)
-            myButton.addTarget(self, action: #selector(pressedButton), for: .touchUpInside)
+            myButton.addTarget(self, action: #selector(flipCard), for: .touchUpInside)
             myButton.tag = cardIndex
             cardIndex += 1
             self.view.addSubview(myButton)
             
-            let localCard = Card(button: myButton, buttonTitle: titleOfCard, matched: false)
+            let localCard = Card(button: myButton, buttonTitle: titleOfCard, matched: false, touched: false)
             cards.append(localCard)
         }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.setupCards()
+    }
+    
+    @objc func flipCard(on button: UIButton)
+    {
+        if (!isFlippingCurrently) {
+            
+            numberOfTouchedCard += 1
+            
+            let cardIndex = button.tag - 1;
+            
+            if (cardIndex != previousCardIndex) {
+                
+                var card = cards[cardIndex];
+                card.touched = true;
+                
+                let cardButton = card.button;
+                cardButton.setTitle(card.buttonTitle, for: UIControlState.normal)
+                cardButton.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+                
+                if (numberOfTouchedCard == 1) {
+                    previousCardIndex = cardIndex;
+                }
+                
+                if (numberOfTouchedCard >= 2) {
+                    isFlippingCurrently = true;
+                    if (cards[previousCardIndex].buttonTitle == cards[cardIndex].buttonTitle) {
+                        cards[previousCardIndex].matched = true;
+                        cards[cardIndex].matched = true;
+                    }
+                
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        self.flipAllCardsBackwards();
+                        
+                        var totalCardMatched = 0;
+                        
+                        for card in self.cards {
+                            if (card.matched == true) {
+                                totalCardMatched += 1;
+                            }
+                        }
+                        
+                        if (totalCardMatched >= self.numberOfCards) {
+                            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                self.increaseNumberOfCards();
+                            }
+                        }
+                        
+                        self.isFlippingCurrently = false
+                    }
+                    
+                    numberOfTouchedCard = 0
+                }
+            }
         }
     }
     
-    func setupCards()
+    func increaseNumberOfCards()
     {
         for card in cards {
-            let btn = card.button
-            btn.setTitle("", for: UIControlState.normal)
-            btn.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            card.button.removeFromSuperview();
         }
+        cards.removeAll();
+        
+        numberOfCards += 2
+        if (numberOfCards > 10) {
+            numberOfCards = 10
+        }
+        createCards(by: numberOfCards)
     }
     
-    @objc func pressedButton(on button: UIButton)
+    func flipAllCardsBackwards()
     {
-        print(firstPick)
-        
-        
-        print(secondPick)
-        
-        let touchedCard = cards[button.tag]
-        let touchedButton = touchedCard.button
-        
-        if (firstPick == nil) && (secondPick == nil) {
-            firstPick = touchedCard
-        }
-        
-        else if (firstPick != nil) && (secondPick == nil) {
-            secondPick = touchedCard
-        }
-        
-        print("FP: " + firstPick.buttonTitle)
-        
-        print("SP: " + secondPick.buttonTitle)
-        
-        print("pressed " + button.currentTitle!)
-        if (touchedButton.currentTitle == "")
-        {
-            print(button.tag)
-            button.setTitle(button.currentTitle!, for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
-        }
-        
-        else {
-            print(button.tag)
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        for card in cards {
+            if (card.matched == false ) {
+                let buttonn = card.button
+                buttonn.setTitle("", for: UIControlState.normal)
+                buttonn.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            }
         }
     }
     
     override func viewDidLoad() {
-        
+        createCards(by: numberOfCards)
     }
-
 }
 
